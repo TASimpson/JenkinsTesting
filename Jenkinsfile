9f2ddb0cd900a680899e6da52188a18e74bca1a1
unknown <tania.simpson@gmail.com>
2021-10-07 08:12:43 -0500

    slack message script 1

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,10 +13,13 @@ pipeline {
         }
         stage('Slack it!') {
             steps {
+                echo "---Testing Sending Slack Message---"
+                slackSend channel: '#jenkins', message: 'Hello From Jenkinsfile!'
+
                 script{
                     echo "---Dig into slack script---"
-                    mainJenkinsFile = load "helpers/slack.groovy"
-                    mainJenkinsFile.runJob()                   
+                    def slackRunner = load "helpers/slack.groovy"
+                    slackRunner.slackNotification()                   
                 }
             }
         }
--- helpers/slack.groovy
+++ helpers/slack.groovy
@@ -1,22 +1,28 @@
-def runJob() {
-    pipeline {
-        agent any
-        stages {
-            stage('Sending Slack Message From Groovy File') {
-                steps {
-                    echo "---Testing Sending Slack Message---"
-                    slackSend channel: '#jenkins', message: 'Hello Again!'
-                }
-            }
-        }
+def slackNotification() {
+    // pipeline {
+    //     agent any
+    //     stages {
+    //         stage('Sending Slack Message From Groovy File') {
+    //             steps {
+    //                 echo "---Testing Sending Slack Message---"
+    //                 slackSend channel: '#jenkins', message: 'Hello Again!'
+    //             }
+    //         }
+    //     }
         
-        post {
-            failure {
-                echo "Job Failed"
-            }
-        }
-    }
+    //     post {
+    //         failure {
+    //             echo "Job Failed"
+    //         }
+    //     }
+    // }
 
+    try {
+        echo "---Testing Sending Slack Message---"
+        slackSend channel: '#jenkins', message: 'Hello From Groovy Script!'
+    } catch (e) {
+        echo "---We Failed---"
+    }
 }
 
 return this
